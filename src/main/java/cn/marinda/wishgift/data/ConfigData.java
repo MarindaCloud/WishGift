58f1130a7af341ce62a4fdc9bb61fd188c0284b7
Administrator <2398648561@qq.com>
2021-02-22 19:39:53 +0800

    2021 VexView component defaultss

--- src/main/java/cn/marinda/wishgift/data/ConfigData.java
+++ src/main/java/cn/marinda/wishgift/data/ConfigData.java
@@ -13,8 +13,6 @@ public class ConfigData {
     private List<String> totalWishPrizeImg = new ArrayList<>();
     private Map<String,PointsLuckyData> luckyData = new HashMap<>();
     public ConfigData(){
-
-
     }
 
 }
