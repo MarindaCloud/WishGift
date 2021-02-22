fe5cf4f169d00d7bed7e751b6243da704ffeebe7
Administrator <2398648561@qq.com>
2021-02-22 09:56:56 +0800

    2021 plugin default

--- src/main/java/cn/marinda/wishgift/commands/ShowCommand.java
+++ src/main/java/cn/marinda/wishgift/commands/ShowCommand.java
@@ -7,7 +7,6 @@ public class ShowCommand extends WishGiftCommands{
     public boolean WishGiftCommands(String[] args, String label) {
         if(label.equalsIgnoreCase("show")){
             if(args[0].equalsIgnoreCase(label)){
-                System.out.println("本次执行的是：show方法");
                 return true;
             }
         }
